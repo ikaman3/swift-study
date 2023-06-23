a83a895a2fe07fa0210bd36cb7387d3be67077be
ikaman3 <yjhama1532@gmail.com>
2023-06-23 14:29:06 +0900

    myfirst 첫 push

---
+++ myfirst/myfirst/myfirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfirstApp.swift
+//  myfirst
+//
+//  Created by main on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct myfirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
