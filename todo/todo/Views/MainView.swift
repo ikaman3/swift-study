4e39b5202a033224732a94eaecdc914d90439da0
ikaman3 <yjhama1532@gmail.com>
2023-07-31 21:51:05 +0900

    todo 만듦. 엔터 누르면 텍스트 필드 초기화가 안 됨

--- todo/todo/Views/MainView.swift
+++ todo/todo/Views/MainView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var newTodo: String = ""
+    @State private var todos: [String] = []
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                TextField("Enter a new todo", text: $newTodo, onCommit: addTodo)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(height: 50)
+                Button("Add") {
+                    addTodo()
+                }
+                .padding()
+            }
+            
+            Button("Clear") {
+                clearTodos()
+            }
+            
+            List {
+                ForEach(todos, id: \.self) { todo in
+                    HStack {
+                        Text(todo)
+                        Spacer()
+                        Button(action: {
+                            deleteTodo(todo)
+                        }) {
+                            Image(systemName: "trash")
+                        }
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    func addTodo() {
+        if !newTodo.isEmpty {
+            todos.append(newTodo)
+            newTodo = ""
+        }
+    }
+    
+    func deleteTodo(_ todo: String) {
+        if let index = todos.firstIndex(of: todo) {
+            todos.remove(at: index)
+        }
+    }
+    
+    func clearTodos() {
+        todos = [];
+        newTodo = ""
     }
 }
 
